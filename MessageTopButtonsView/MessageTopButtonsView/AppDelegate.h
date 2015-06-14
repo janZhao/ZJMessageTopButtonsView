b18cb0a60caba2e292298dcefd951007093f183a
janZhao <zhaojian3513@163.com>
2015-06-14 11:12:12 +0800

    初始化项目

---
+++ MessageTopButtonsView/MessageTopButtonsView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageTopButtonsView
+//
+//  Created by jyd on 15/6/14.
+//  Copyright (c) 2015年 jyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessageTopButtonsView/MessageTopButtonsView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MessageTopButtonsView
+//
+//  Created by jyd on 15/6/14.
+//  Copyright (c) 2015年 jyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
